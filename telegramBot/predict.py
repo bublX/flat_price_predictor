50b10e3d66ab1957e1c0d4403e3d80c16f61ab24
nikita <nkartemev@edu.hse.ru>
2026-06-27 00:40:16 +0300

    fix 2 bugs telegram_bot

--- telegramBot/predict.py
+++ telegramBot/predict.py
@@ -24,11 +24,10 @@ def predict_price(area: float, rooms: int, kitchen_area: float,
                   levels: int, floor_ratio: float,
                   building_type: int, object_type: int, id_region: int,
                   month: int, quarter: int, season: int, day_of_week: int) -> float:
-    input_data = pd.DataFrame([[
+    input_array = np.array([[
         area, rooms, kitchen_area, levels,
         floor_ratio, building_type, object_type,
         id_region, month, quarter, season, day_of_week
-    ]], columns=FEATURE_ORDER)
-
-    raw_pred = model.predict(input_data)[0]
+    ]])
+    raw_pred = model.predict(input_array)[0]
     return np.exp(raw_pred)
